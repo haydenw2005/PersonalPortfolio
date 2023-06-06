7a580fae6be0d0e1d91aed584a46476723d63057
haydenw2005 <haydenswhite@gmail.com>
2023-06-05 18:31:59 -0700

    shifting diaganols

--- static/scripts/transformlines.js
+++ static/scripts/transformlines.js
@@ -2,20 +2,41 @@ const borderRatio = 0.165
 const startMovementRatio = 0.1;  // 0.1 + 17.5vh
 
 document.addEventListener('DOMContentLoaded', function() {
+  adjustSlantLine(0);
   let scrollableElement = document.getElementById('static-side-borders');
   let lastYPos = 0;
   scrollableElement.addEventListener('scroll', function() {
     isUp = lastYPos < scrollY;
     scrollY = scrollableElement.scrollTop;
 
-    moveBottomLines(scrollY, isUp)
+    moveBottomLines(scrollY, isUp);
     
-  
     lastYPos = scrollY;
     
   });
 
 });
+
+window.addEventListener('resize', function() {
+  // Call your JavaScript function here
+  adjustSlantLine(0);
+});
+
+
+function adjustSlantLine(i, h) {
+  let movingSlantLines = document.getElementsByClassName('slant-line');
+  currentObj = movingSlantLines[i];
+  let respectiveBorder = document.getElementsByClassName('border');
+  let w = respectiveBorder[i].offsetWidth/3;
+  let hypo = (w**2+h**2)**(1/2);
+  currentObj.style.width = hypo + 'px';
+  angleDeg = Math.acos(w/hypo) * (180 / Math.PI);
+  if (i % 2 == 1) angleDeg = angleDeg * -1;
+  currentObj.style.transform = 'translateY(' + (h/2) + 'px)' + ' rotate(' + angleDeg + 'deg)';
+  currentObj.style.left = w + 'px';
+
+}
+
 
 function moveBottomLines(scrollY, isUp) {
   let movingBottomLines = document.getElementsByClassName('bottom-pos');
@@ -32,14 +53,16 @@ function moveBottomLines(scrollY, isUp) {
 
       if (isUp) {
         if (scrollY > yPos-windowHeight && -(distanceHeight) > (scrollY-origYVal)) {
-          currentObj.style.transform = 'translateY(' + (scrollY - origYVal+windowHeight) + 'px)';
+          currentObj.style.transform = 'translateY(' + (yPos - origYVal+windowHeight) + 'px)';
         }
       } else {
         if (-(distanceHeight) > (scrollY-origYVal) && scrollY > origYVal-windowHeight) {
           currentObj.style.transform = 'translateY(' + (scrollY - (origYVal-windowHeight) ) + 'px)';
-          console.log(scrollY - (origYVal-distanceHeight));
         }
       }
+      adjustSlantLine(i, yPos-origYVal);
+
+      
 
     }
 }
