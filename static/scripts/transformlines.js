ca2143b5c0cd5be57e0bdce46f441d33201bdf0d
haydenw2005 <haydenswhite@gmail.com>
2023-06-07 15:48:09 -0700

    cleanup

--- static/scripts/transformlines.js
+++ static/scripts/transformlines.js
@@ -1,49 +1,45 @@
 const borderRatio = 0.165
-const startMovementRatio = 0.1;  // 0.1 + 17.5vh
+const startMovementRatio = 0.1;
+const respectiveBorder = document.getElementsByClassName('border');
 
 document.addEventListener('DOMContentLoaded', function() {
   adjustSlantLine(0);
   let scrollableElement = document.getElementById('static-side-borders');
   let lastYPos = 0;
   scrollableElement.addEventListener('scroll', function() {
-    isUp = lastYPos < scrollY;
     scrollY = scrollableElement.scrollTop;
-
+    isUp = lastYPos < scrollY;
+    
     moveBottomLines(scrollY, isUp);
-    
     lastYPos = scrollY;
-    
   });
-
 });
 
+
 window.addEventListener('resize', function() {
-  // Call your JavaScript function here
   adjustSlantLine(0);
 });
 
 
 function adjustSlantLine(i, h) {
   let movingSlantLines = document.getElementsByClassName('slant-line');
-  currentObj = movingSlantLines[i];
-  let respectiveBorder = document.getElementsByClassName('border');
+  let currentObj = movingSlantLines[i];
   let w = respectiveBorder[i].offsetWidth/3;
-  let hypo = (w**2+h**2)**(1/2);
-  currentObj.style.width = hypo + 'px';
-  angleDeg = Math.acos(w/hypo) * (180 / Math.PI);
+  let hypo = Math.sqrt(w ** 2 + h ** 2);
+  currentObj.style.width = (hypo * 1.01) + 'px';
+  angleDeg = Math.acos(w / hypo) * (180 / Math.PI);
   if (i % 2 == 1) angleDeg = angleDeg * -1;
   currentObj.style.transform = 'translateY(' + (h/2) + 'px)' + ' rotate(' + angleDeg + 'deg)';
-  currentObj.style.left = w + 'px';
-
+  currentObj.style.left = (w - (hypo*1.01-w)/2) + 'px';
 }
 
 
 function moveBottomLines(scrollY, isUp) {
   let movingBottomLines = document.getElementsByClassName('bottom-pos');
     for (let i = 0; i < movingBottomLines.length; i++) {
-      currentObj = movingBottomLines[i];
+      let currentObj = movingBottomLines[i];
       
-      yPos = getYPos(currentObj)
+      let yPos = getYPos(currentObj)
 
       let distanceHeight = window.innerHeight * startMovementRatio;
       let borderHeight = window.innerHeight * borderRatio
@@ -51,32 +47,26 @@ function moveBottomLines(scrollY, isUp) {
 
       let origYVal = currentObj.offsetTop;
 
-      if (isUp) {
-        if (scrollY > yPos-windowHeight && -(distanceHeight) > (scrollY-origYVal)) {
-          currentObj.style.transform = 'translateY(' + (yPos - origYVal+windowHeight) + 'px)';
-        }
-      } else {
-        if (-(distanceHeight) > (scrollY-origYVal) && scrollY > origYVal-windowHeight) {
-          currentObj.style.transform = 'translateY(' + (scrollY - (origYVal-windowHeight) ) + 'px)';
-        }
+      if (scrollY > origYVal-windowHeight && -(distanceHeight) > (scrollY-origYVal)) {
+        currentObj.style.transform = 'translateY(' + (scrollY - (origYVal-windowHeight) ) + 'px)';
       }
-      adjustSlantLine(i, yPos-origYVal);
-
       
-
+      adjustSlantLine(i, yPos-origYVal);
     }
 }
+
 
 function getYPos(obj) {
   let computedStyle = window.getComputedStyle(obj);
   let transformValue = computedStyle.getPropertyValue('transform');
+  let yPos = 0;
   if (transformValue != 'none') {
     let transformMatrix = transformValue.split('(')[1].split(')')[0].split(',');
     let transY = transformMatrix[transformMatrix.length - 1];
     yPos = obj.offsetTop + parseInt(transY);
   }
   else {
-    yPos = obj.offsetTop
+    yPos = obj.offsetTop;
   }
   return yPos;
 }
